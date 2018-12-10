fc9d3974143c0a4a9efd7fa70a37a0156a07f923
Matthias Wurm <wurm.matthias@gmx.net>
2018-12-10 18:38:13 +0100

    Portfolio plus intro bg

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-    sectionsColor : ['#0798ec', '#fec401'],
+    sectionsColor : ['#0798ec', '#fec401', '#B4D455'],
+    anchors:['intro', 'portfolio', 'contact']
   });
 
 });
