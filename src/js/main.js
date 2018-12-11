8d3cf65436dd2144bbd3a66af710db9bc68374f6
Matthias Wurm <wurm.matthias@gmx.net>
2018-12-11 08:12:18 +0100

    Contact Links & Portfolio update

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-    sectionsColor : ['#0798ec', '#fec401', '#B4D455'],
-    anchors:['intro', 'portfolio', 'contact']
+    sectionsColor : ['#0798ec', '#fec401', '#B4D455', '#0798ec'],
+    anchors:['intro', 'portfolio', 'contact', 'footer']
   });
 
 });
